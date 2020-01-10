e30d3cac343177e74c9af5ba9b727c88cfbefff8
Christine Lamitina <christinelamitina@gmail.com>
2020-01-10 11:40:09 -0600

    bootstrap nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Navbar, NavItem, NavLink, NavbarBrand } from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Navbar dark color="dark" expand="lg">
+                <div className="container">
+                    <NavbarBrand href="/">The Mandalorian</NavbarBrand>
+                    <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+                </div>
+                </Navbar>
+            </div>
+        );
+    }
 }
 
 export default App;
